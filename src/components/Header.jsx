c1e08872ca2aa236a10f5534409b720e0f56f8fb
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-10-02 17:35:49 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div>
+      {/* Navigation bar container */}
       <nav className="flex items-center justify-between h-20 max-w-6xl mx-auto">
+        {/* Link to homepage */}
         <Link to={"/"}>
           <div className="ml-5">
             <h1 className="text-red-900 font-bold text-xl sm:text-2xl md:text-4xl cursor-pointer tracking-wide">
@@ -11,10 +13,13 @@ export default function Header() {
             </h1>
           </div>
         </Link>
+        {/* Navigation links */}
         <ul className="flex list-none items-center space-x-6 text-black text-2xl font-semibold">
+          {/* Link to home */}
           <Link to={"/"}>
             <li className="cursor-pointer">Home</li>
           </Link>
+          {/* Link to cart */}
           <Link to={"/cart"}>
             <li className="cursor-pointer">Cart</li>
           </Link>
