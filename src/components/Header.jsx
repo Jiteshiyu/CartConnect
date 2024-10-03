4b745018a3c78937ed7c45b114dcff2f3d5f6a51
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-10-03 08:08:57 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,13 +8,13 @@ export default function Header() {
         {/* Link to homepage */}
         <Link to={"/"}>
           <div className="ml-5">
-            <h1 className="text-red-900 font-bold text-xl sm:text-2xl md:text-4xl cursor-pointer tracking-wide">
+            <h1 className="text-red-900 font-bold text-2xl cursor-pointer tracking-wide">
               CartConnect
             </h1>
           </div>
         </Link>
         {/* Navigation links */}
-        <ul className="flex list-none items-center space-x-6 text-black text-2xl font-semibold">
+        <ul className="flex list-none items-center space-x-6 text-black text-2xl font-semibold mr-5">
           {/* Link to home */}
           <Link to={"/"}>
             <li className="cursor-pointer">Home</li>
